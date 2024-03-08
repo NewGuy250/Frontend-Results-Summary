77ebff74d87a0c214e78a5fe8bd6b874d1e64eb3
NewGuy250 <ndaniel220@yahoo.com>
2024-03-08 15:30:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // Function to populate HTML elements with JSON data
 function populateSummary(data) {
+  let totalScore = 0;
   data.forEach(item => {
     const categoryElement = document.querySelector(`.${item.category.toLowerCase()}-score`);
     categoryElement.querySelector('.black-text').textContent = item.score;
+    totalScore += item.score;
     categoryElement.querySelector('img').src = item.icon;
   });
+
+  // Calculate the average score
+  const averageScore = totalScore / data.length;
+
+  // Update the .score element with the average score
+  document.querySelector('.score').textContent = Math.round(averageScore);
 }
 
 // Fetch JSON data from file
